2150f93b0e4cda65485052721ca7d5089b67a98d
Christian-daigneault <72392123+Christian-daigneault@users.noreply.github.com>
2021-01-02 15:35:16 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/file/d/1DbfpfGKFJOBVNUnR7e6PzUv0hJDn-aDm/view?usp=sharing'
 export_file_name = 'export.pkl'
 
 classes = ['Van Gogh', 'Monet', 'Matisse']
