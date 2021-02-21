5df26d0dcc6c8ebbb2b4560a31b983f519c661fa
Christian-daigneault <72392123+Christian-daigneault@users.noreply.github.com>
2021-02-21 19:13:41 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url ='https://www.dropbox.com/s/qauhxrjycuinjc1/export.pkl?dl=1'
-export_file_name = 'export.pkl'
+export_file_url ='https://www.dropbox.com/s/6zo6cd2szs6yjmr/export-3.pkl?dl=0'
+export_file_name = 'export-3.pkl'
 
 classes = ['monet','vangogh','picasso']
 path = Path(__file__).parent
